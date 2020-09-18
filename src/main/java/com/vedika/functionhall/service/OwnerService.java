630e0f4c3f2c295768c9cb84000a54a04a8fb6a9
devendra-suntechno <k.devendra@suntechnoservices.com>
2020-09-18 16:50:52 +0530

    vedikaservice 18-09

---
+++ src/main/java/com/vedika/functionhall/service/OwnerService.java
@@ -0,0 +1,21 @@
+package com.vedika.functionhall.service;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.vedika.functionhall.model.Owner;
+
+@Service
+public interface OwnerService {
+
+	Owner saveOrUpdateOwner(Owner owner);
+
+	List<Owner> findFunctionHallByNameAndCity(String city, String name);
+
+	void update(String correlationid, String imageUrl) throws FileNotFoundException, RuntimeException;
+
+	List<Owner> findByownerID(String _id);
+
+}
